fb04b61b0ffa44577a04407690bc9f1fadeae17a
Ryan Leckey <rleckey@concordusapps.com>
2013-04-03 05:58:59 -0700

    Version 6 for python six.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'wsgi_intercept',
-    version = '0.0.1',
+    version = '0.6.0',
     author = 'Concordus Applications, Titus Brown, Kumar McMillan, Chris Dent',
     author_email = 'cdent@peermore.com',
     license = 'MIT License',
